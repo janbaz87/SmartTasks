dbd9ee1e47041d891264b26a6ce70006b3c35ec6
Janbaz Ali <janbaz.ali@getir.com>
2024-06-07 17:25:03 +0500

    Implement smart colors

--- SmartTasks/SmartTasks/Core/SmartColors/SmartColors.swift
+++ SmartTasks/SmartTasks/Core/SmartColors/SmartColors.swift
@@ -5,4 +5,34 @@
 //  Created by Janbaz Ali on 07/06/2024.
 //
 
-import Foundation
+import UIKit
+
+public typealias SmartColor = UIColor
+
+// MARK: Smart Colors
+
+extension SmartColor {
+    class var appBackground: UIColor {
+        return colorWith(name: "smart_background")
+    }
+
+    class var red: UIColor {
+        return colorWith(name: "smart_red")
+    }
+
+    class var green: UIColor {
+        return colorWith(name: "smart_green")
+    }
+
+    class var OffWhite: UIColor {
+        return colorWith(name: "smart_cream")
+    }
+
+    static func colorWith(name : String ) -> UIColor {
+        guard let color =  UIColor(named: name) else {
+            return .red
+        }
+
+        return color
+    }
+}
