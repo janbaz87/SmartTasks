72e8a2d720072f7aa8490749a37110bc228f334b
Janbaz Ali <janbaz.ali@getir.com>
2024-06-09 01:21:18 +0500

    Add app configuration

---
+++ SmartTasks/SmartTasks/App/AppConfiguration/AppConfiguration.swift
@@ -0,0 +1,11 @@
+//
+//  AppConfiguration.swift
+//  SmartTasks
+//
+//  Created by Janbaz Ali on 08/06/2024.
+//
+
+enum AppConfiguration {
+    static let baseURL = "http://demo1414406.mockable.io/"
+}
+
