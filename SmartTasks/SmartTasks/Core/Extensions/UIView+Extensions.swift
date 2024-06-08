8b759747997060bb0d4f3cbea390cf506a961a84
Janbaz Ali <janbaz.ali@getir.com>
2024-06-09 02:56:13 +0500

    Implement extensions

--- SmartTasks/SmartTasks/Core/Extensions/UIView+Extensions.swift
+++ SmartTasks/SmartTasks/Core/Extensions/UIView+Extensions.swift
@@ -5,4 +5,37 @@
 //  Created by Janbaz Ali on 09/06/2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    public var cornerRadius: CGFloat {
+        get {
+            layer.cornerRadius
+        }
+
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    public var borderWidth: CGFloat {
+        get {
+            layer.borderWidth
+        }
+
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    public var borderColor: UIColor? {
+        get {
+            UIColor(cgColor: layer.borderColor!)
+        }
+
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
