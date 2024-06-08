74b92da967adfcfef16aa123f50b59a4aa754e29
Janbaz Ali <janbaz.ali@getir.com>
2024-06-09 02:56:34 +0500

    Implement reusable view

--- SmartTasks/SmartTasks/Core/Reusable/ReusableView.swift
+++ SmartTasks/SmartTasks/Core/Reusable/ReusableView.swift
@@ -5,4 +5,18 @@
 //  Created by Janbaz Ali on 09/06/2024.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - ReusableView
+
+/// ReusableView protocol.
+public protocol ReusableView: AnyObject { }
+
+/// ReusableView extension, Provides reuseIdentifier from class name.
+extension ReusableView where Self: UIView {
+    public static var reuseIdentifier: String {
+        String(describing: self)
+    }
+
+    public func prepareForReuse() { }
+}
