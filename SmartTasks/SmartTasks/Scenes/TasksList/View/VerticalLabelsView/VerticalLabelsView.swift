5c97dfa7c0ace01e13b8b530d472a9ac9602f848
Janbaz Ali <janbaz.ali@getir.com>
2024-06-09 02:58:00 +0500

    Implement vertical labels

--- SmartTasks/SmartTasks/Scenes/TasksList/View/VerticalLabelsView/VerticalLabelsView.swift
+++ SmartTasks/SmartTasks/Scenes/TasksList/View/VerticalLabelsView/VerticalLabelsView.swift
@@ -5,4 +5,62 @@
 //  Created by Janbaz Ali on 09/06/2024.
 //
 
-import Foundation
+import UIKit
+
+class VerticalLabelsView: UIView {
+
+    // Title label
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = SmartFont.font(withName: .amsiProRegular, size: 10)
+        label.textColor = .gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // Value label
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.font = SmartFont.font(withName: .amsiProBold, size: 15)
+        label.textColor = SmartColor.appRed
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // Custom initializer
+    init() {
+        super.init(frame: .zero)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setTitle(title: String, value: String) {
+        // Set the label texts
+        titleLabel.text = title
+        valueLabel.text = value
+    }
+
+    // Setup the view
+    private func setupView() {
+        // Add labels to the view
+        addSubview(titleLabel)
+        addSubview(valueLabel)
+
+        // Setup Auto Layout constraints
+        NSLayoutConstraint.activate([
+            // Title label constraints
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+
+            // Value label constraints
+            valueLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            valueLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            valueLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            valueLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
