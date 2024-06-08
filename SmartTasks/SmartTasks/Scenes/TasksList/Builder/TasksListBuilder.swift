dbccd5d304fa26837d0649e2f708b881735f993d
Janbaz Ali <janbaz.ali@getir.com>
2024-06-09 01:22:15 +0500

    Inject service to interactor

--- SmartTasks/SmartTasks/Scenes/TasksList/Builder/TasksListBuilder.swift
+++ SmartTasks/SmartTasks/Scenes/TasksList/Builder/TasksListBuilder.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Networking
 
 protocol TasksListBuildable {
     func buildTasksList(router: TasksListRouting) -> TasksListViewController
@@ -14,7 +15,8 @@ protocol TasksListBuildable {
 class TasksListBuilder: TasksListBuildable {
 
     func buildTasksList(router: TasksListRouting) -> TasksListViewController {
-        let interactor = TasksListInteractor()
+        let networking = Networking()
+        let interactor = TasksListInteractor(service: TasksService(networking: networking))
         let presenter = TasksListPresenter(
             interactor: interactor,
             router: router
