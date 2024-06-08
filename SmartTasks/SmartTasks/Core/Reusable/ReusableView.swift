b2b4bf21b426fb430c254a25cf907d57a1201138
Janbaz Ali <janbaz.ali@getir.com>
2024-06-09 02:54:51 +0500

    Add extensions

---
+++ SmartTasks/SmartTasks/Core/Reusable/ReusableView.swift
@@ -0,0 +1,8 @@
+//
+//  ReusableView.swift
+//  SmartTasks
+//
+//  Created by Janbaz Ali on 09/06/2024.
+//
+
+import Foundation
---
+++ SmartTasks/SmartTasks/Scenes/TasksList/View/Cell/TaskCollecCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TaskCollecCollectionViewCell.swift
+//  SmartTasks
+//
+//  Created by Janbaz Ali on 09/06/2024.
+//
+
+import UIKit
+
+class TaskCollecCollectionViewCell: UICollectionViewCell {
+    
+}
