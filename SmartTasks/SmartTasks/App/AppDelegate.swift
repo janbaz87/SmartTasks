ff63ef5d305ecf5b73df8dfdf4234808f3fcc639
Janbaz Ali <janbaz.ali@getir.com>
2024-06-10 01:49:55 +0500

    Set nav bar appearance

--- SmartTasks/SmartTasks/App/AppDelegate.swift
+++ SmartTasks/SmartTasks/App/AppDelegate.swift
@@ -10,10 +10,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setNavigationBarAppearance()
         return true
     }
 
@@ -31,6 +30,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    private func setNavigationBarAppearance() {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ]
+        navigationBarAppearance.backgroundColor = SmartColor.appBackground
+        
+        // Apply the appearance settings to the navigation bar
+        UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+        UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+    }
 
 }
 
